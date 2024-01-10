71196dae4235e4e987976002c5b070e89c6c938e
Natalia DaLomba <natalia@starforce.games>
2024-01-10 12:39:40 -0500

    WIP

--- Assets/Scripts/InactivityDetector.cs
+++ Assets/Scripts/InactivityDetector.cs
@@ -9,6 +9,9 @@ public class InactivityDetector : MonoBehaviour {
     public float inactivityThreshold = 5f;
     public float lastInputTime;
     private SwitchCameras switchCams;
+    private float sequenceLength = 5;
+    private float sequenceCount = 6;
+    private float currentTime = 123;
 
     private bool isActive => Input.anyKey || Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0;
 
@@ -24,16 +27,26 @@ public class InactivityDetector : MonoBehaviour {
         if (isActive) {
             lastInputTime = Time.time;
             switchCams.enabled = true;
-            timeline.Stop();
+            timeline.Play();
+            timeline.time = (sequenceLength * Mathf.Ceil(currentTime / sequenceLength)) % (sequenceLength * (sequenceCount + 1));
             secUntilIdle.ResetCountdown();
         }
         else {
             // If no input for inactivityThreshold seconds
+            //This math makes a pattern that rounds up to the next sequence's start time,
+            //Wrapping back around to t = 0 after we've passed all the sequences.
+            //      __      __
+            //    __      __
+            //  __      __
+            //__      __
             if (Time.time - lastInputTime > inactivityThreshold) {
                 switchCams.DeactivateCamera(switchCams.currentCamIndex);
                 switchCams.enabled = false;
+                timeline.time = 0;
                 timeline.Play();
             }
         }
     }
+
+
 }
