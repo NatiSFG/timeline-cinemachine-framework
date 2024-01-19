14f77cd5c7b0faf80586562b69f70d4219856bf7
Natalia DaLomba <natalia@starforce.games>
2024-01-18 21:43:46 -0600

    The player can accelerate/decelerate their movement speed by holding/releasing the left shift key

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -1,21 +1,22 @@
 using UnityEngine;
 
 public class ShipController : MonoBehaviour {
-    [SerializeField] private float moveSpeed = 50f;
-    [SerializeField] private float moveSpeedBoostMultiplier = 1.5f;
+    [SerializeField] private float currentMoveSpeed = 50f;
     [SerializeField] private float rotSpeed = 90f;
 
-    private float baseMoveSpeed;
+    [SerializeField] private float boostMoveSpeedMult = 1.5f;
+
+    private float baseMoveSpeed = 50f;
 
     private void Awake() {
-        baseMoveSpeed = moveSpeed;
+        currentMoveSpeed = baseMoveSpeed;
     }
 
     void Update() {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * currentMoveSpeed * Time.deltaTime;
         transform.Translate(movement);
 
         // Yaw Adjustment (Rotation around Y-axis)
@@ -39,10 +40,10 @@ public class ShipController : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.LeftShift))
             SpeedBoost();
-        else moveSpeed = baseMoveSpeed;
+        else currentMoveSpeed = baseMoveSpeed;
     }
 
     private void SpeedBoost() {
-        moveSpeed *= moveSpeedBoostMultiplier;
+        currentMoveSpeed = baseMoveSpeed * boostMoveSpeedMult;
     }
 }
