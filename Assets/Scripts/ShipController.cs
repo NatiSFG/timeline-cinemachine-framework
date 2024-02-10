e9131c764c9092166f576130c61036168c3d62d1
Natalia DaLomba <natalia@starforce.games>
2024-02-10 07:50:11 -0600

    Commit before major changes to all axes for Yaw, Pitch and Roll on ship movement

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour {
     [SerializeField] private float currentMoveSpeed = 50f;
-    [SerializeField] private float rotSpeed = 90f;
+    [SerializeField] private float yawPitchSpeed = 90f;
 
     [SerializeField] private float boostMoveSpeedMult = 1.5f;
 
@@ -13,29 +13,34 @@ public class ShipController : MonoBehaviour {
     }
 
     void Update() {
+        CalculateMovement();
+    }
+
+    private void CalculateMovement() {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * currentMoveSpeed * Time.deltaTime;
         transform.Translate(movement);
 
-        // Yaw Adjustment (Rotation around Y-axis)
+        // Yaw Adjustment (Rotation around Y-axis) Q/E keys
         float yawInput = Input.GetAxis("Yaw");
-        float yawRotation = yawInput * rotSpeed * Time.deltaTime;
+        float yawRotation = yawInput * yawPitchSpeed * Time.deltaTime;
         transform.Rotate(Vector3.up, yawRotation);
 
-        // Pitch Adjustment (Rotation around X-axis)
+        // Pitch Adjustment (Rotation around X-axis) Up/Down arrows
         float pitchInput = Input.GetAxis("Pitch");
-        float pitchRotation = -pitchInput * rotSpeed * Time.deltaTime; // Inverted for natural input
+        float pitchRotation = -pitchInput * yawPitchSpeed * Time.deltaTime; // Inverted for natural input
         transform.Rotate(Vector3.right, pitchRotation);
 
-        // Roll Adjustment (Rotation around Z-axis)
-        if (Input.GetKey(KeyCode.Q)) {
-            float rollRotation = rotSpeed * Time.deltaTime;
-            transform.Rotate(Vector3.forward, rollRotation);
-        } else if (Input.GetKey(KeyCode.E)) {
-            float rollRotation = -rotSpeed * Time.deltaTime;
-            transform.Rotate(Vector3.forward, rollRotation);
+        // Roll Adjustment (Rotation around Z-axis) Left/Right arrows
+        float rollInput = Input.GetAxis("Roll");
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            float rollRotation = yawPitchSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.forward, rollRotation, Space.World);
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            float rollRotation = -yawPitchSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.forward, rollRotation, Space.World);
         }
 
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
