be5de93a23f219a01497076be29c16fb7c75cc4f
Natalia DaLomba <natalia@starforce.games>
2024-02-10 17:33:22 -0600

    WIP fixing roll

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour {
     [SerializeField] private float currentMoveSpeed = 50f;
-    [SerializeField] private float yawPitchSpeed = 90f;
+    [SerializeField] private float rotSpeed = 90f;
 
     [SerializeField] private float boostMoveSpeedMult = 1.5f;
 
@@ -24,24 +24,19 @@ public class ShipController : MonoBehaviour {
         transform.Translate(movement);
 
         // Yaw Adjustment (Rotation around Y-axis) Q/E keys
-        float yawInput = Input.GetAxis("Yaw");
-        float yawRotation = yawInput * yawPitchSpeed * Time.deltaTime;
-        transform.Rotate(Vector3.up, yawRotation);
+        //float yawinput = Input.GetAxis("Yaw");
+        //float yawrotation = yawinput * rotSpeed * Time.deltaTime;
+        //transform.Rotate(Vector3.up, yawrotation);
 
         // Pitch Adjustment (Rotation around X-axis) Up/Down arrows
-        float pitchInput = Input.GetAxis("Pitch");
-        float pitchRotation = -pitchInput * yawPitchSpeed * Time.deltaTime; // Inverted for natural input
-        transform.Rotate(Vector3.right, pitchRotation);
+        //float pitchInput = Input.GetAxis("Pitch");
+        //float pitchRotation = -pitchInput * rotSpeed * Time.deltaTime; // Inverted for natural input
+        //transform.Rotate(Vector3.right, pitchRotation);
 
         // Roll Adjustment (Rotation around Z-axis) Left/Right arrows
-        float rollInput = Input.GetAxis("Roll");
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            float rollRotation = yawPitchSpeed * Time.deltaTime;
-            transform.Rotate(Vector3.forward, rollRotation, Space.World);
-        } else if (Input.GetKey(KeyCode.RightArrow)) {
-            float rollRotation = -yawPitchSpeed * Time.deltaTime;
-            transform.Rotate(Vector3.forward, rollRotation, Space.World);
-        }
+        float rollRotation = rotSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow)) transform.Rotate(Vector3.forward, -rollRotation, Space.World);
+        else if (Input.GetKey(KeyCode.RightArrow)) transform.Rotate(Vector3.forward, rollRotation, Space.World);
 
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
             SpeedBoost();
